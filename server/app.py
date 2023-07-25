76a4f79b1f62b3557fecb4734456c81b413bffe5
Travis <travis.williams94@gmail.com>
2023-07-25 16:57:01 -0600

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from flask import Flask, make_response, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
@@ -20,19 +21,75 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict  = {
+            "id": bakery.id,
+            "name": bakery.name,
+            "created_at": bakery.created_at,
+            "updated_at": bakery.updated_at
+        }
+
+        bakeries.append(bakery_dict)
+
+    response = make_response(
+        jsonify(bakeries),
+        200
+    )
+
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(
+        bakery_dict,
+        200
+    )
+    response.headers["Content-Type"] = "application/json"
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price).all()
+    bgoods = []
+
+    for bgood in baked_goods_by_price:
+        bgood_dict = {
+            "id": bgood.id,
+            "name": bgood.name,
+            "price": bgood.price,
+            "created_at": bgood.created_at,
+            "updated_at": bgood.updated_at
+        }
+
+        bgoods.append(bgood_dict)
+
+    response = make_response(
+        jsonify(bgoods),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+
+    most_expensive_dict = most_expensive.to_dict()
+
+    response = make_response(
+        jsonify(most_expensive_dict),
+        200
+    )
+
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
